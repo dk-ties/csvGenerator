f2332d1a6770972777765ff9552cf830856ae0be
dk-ties <dk.ties@gmail.com>
2023-08-22 20:27:16 +0200

    Setting up overall psudocode

---
+++ main.py
@@ -0,0 +1,31 @@
+#Imports############################
+
+#Global variable#######################
+NUMBEROFCOL = ""
+INTERVALDRECORD = ""
+STOPSETPOINT = ""
+
+#Function declaration###################
+#newFile
+def newFile():
+    createnewfile
+    write first line (Header)
+        input to first header
+            for each num in NUMBEROFCOL:
+                write 'col' + num
+    close file
+    save file at newFilepath()
+#locateFile
+open filelocater
+    newFilepath = filelocater
+
+#Loops#########################
+#Generator loop
+input(NUMBEROFCOL)
+input(INTERVALDRECORD)
+input(STOPSETPOINT)
+create new file()
+    setup everything
+ask for confirmation to start generate
+log information to terminal for each generation
+
